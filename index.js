455151618f9b688a75b3dd70629417108f9f20be
Hasnain17360 <onlinehasnain480@gmail.com>
2024-03-22 00:36:58 +0500

    push first project to github

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the following operators to peroform an action:",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Average"],
+    },
+]);
+// Conditional Statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+// for Subtraction
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+// for Multiplication
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+// for Division
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+// for Average
+else if (answer.operator === "Average") {
+    console.log((answer.firstNumber + answer.secondNumber) / 2);
+}
+// other
+else {
+    console.log("Please select valid operator");
+}
